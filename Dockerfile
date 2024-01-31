a168ebc84ca4c42bb14a7805103646558d61c6c2
shanu kumar <shanukumarm@gmail.com>
2024-01-31 17:28:31 +0530

    Improved script and dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 # Use a lightweight base image
-FROM alpine:latest
+FROM alpine:3.19.1
+
+# Create a non-root user to run the application
+RUN adduser -D appuser
 
 # Install curl
 RUN apk --no-cache add curl
@@ -7,8 +10,12 @@ RUN apk --no-cache add curl
 # Copy the bash script into the container
 COPY node_metric_collector.sh /node_metric_collector.sh
 
-# Make the script executable
-RUN chmod +x /node_metric_collector.sh
+# Change ownership and permissions of the script
+RUN chown appuser:appuser /node_metric_collector.sh \
+    && chmod 755 /node_metric_collector.sh
+
+# Switch to the non-root user
+USER appuser
 
 # Run the script when the container starts
 CMD ["sh", "/node_metric_collector.sh"]
--- node_metric_collector.sh
+++ node_metric_collector.sh
@@ -9,12 +9,61 @@ NODE_EXPORTER_URL="http://node-exporter:9100/metrics"
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 METRICS_FILE="$METRICS_DIR/node_metrics_$TIMESTAMP.txt"
 
-# Retrieve CPU, Memory, and Disk metrics from Node Exporter
-CPU_USAGE=$(curl -s $NODE_EXPORTER_URL | grep 'node_cpu_seconds_total' | awk '{print $2}')
-MEMORY_USAGE=$(curl -s $NODE_EXPORTER_URL | grep 'node_memory_MemTotal_bytes' | awk '{print $2}')
-DISK_USAGE=$(curl -s $NODE_EXPORTER_URL | grep 'node_filesystem_size_bytes' | grep 'device="/dev/root"' | awk '{print $2}')
+
+# Retrieve CPU metrics from Node Exporter
+CPU_METRICS=$(curl -s "$NODE_EXPORTER_URL" | grep 'node_cpu_seconds_total')
+
+# Initialize variables for total time and total idle time
+TOTAL_CPU=0
+TOTAL_FREE_CPU=0
+
+# Loop through each CPU core metric
+while IFS= read -r line; do
+    # Extract mode and value from the metric
+    mode=$(echo "$line" | awk -F '[{},="]' '{print $8}')
+    value=$(echo "$line" | awk '{print $2}')
+
+    # If mode is idle, add value to total idle time
+    if [ $mode = "idle" ]; then
+        TOTAL_FREE_CPU=$(awk "BEGIN {print $TOTAL_FREE_CPU + $value}")
+    fi
+
+    # Add value to total time
+    TOTAL_CPU=$(awk "BEGIN {print $TOTAL_CPU + $value}")
+
+done < <(echo "$CPU_METRICS")
+
+# Calculate CPU usage percentage
+CPU_USAGE_PERCENTAGE=$(awk "BEGIN {print (($TOTAL_CPU - $TOTAL_FREE_CPU) / $TOTAL_CPU) * 100}")
+
+
+# Retrieve Memory metrics from Node Exporter
+TOTAL_MEMORY=$(curl -s $NODE_EXPORTER_URL | grep '^node_memory_MemTotal_bytes' | awk '{print $2}')
+FREE_MEMORY=$(curl -s $NODE_EXPORTER_URL | grep '^node_memory_MemFree_bytes' | awk '{print $2}')
+
+# Calculate Memory usage percentage
+MEMORY_USAGE_PERCENTAGE=$(awk "BEGIN {print (($TOTAL_MEMORY - $FREE_MEMORY) / $TOTAL_MEMORY) * 100}")
+
+
+# Retrieve Disk metrics from Node Exporter
+DISK_SIZE=$(curl -s $NODE_EXPORTER_URL | awk -F ' ' '/node_filesystem_size_bytes.*device="tmpfs".*mountpoint="\/var"/ {print $2}')
+FREE_DISK=$(curl -s $NODE_EXPORTER_URL | awk -F ' ' '/node_filesystem_free_bytes.*device="tmpfs".*mountpoint="\/var"/ {print $2}')
+
+# Calculate Memory usage percentage
+DISK_USAGE_PERCENTAGE=$(awk "BEGIN {print (($DISK_SIZE - $FREE_DISK) / $DISK_SIZE) * 100}")
+
 
 # Write metrics to file with timestamped filename
-echo "CPU Usage: $CPU_USAGE" > $METRICS_FILE
-echo "Memory Usage: $MEMORY_USAGE" >> $METRICS_FILE
-echo "Disk Usage: $DISK_USAGE" >> $METRICS_FILE
+cat <<EOF > "$METRICS_FILE"
+Total CPU: $TOTAL_CPU
+Free CPU: $TOTAL_FREE_CPU
+CPU Usage Percentage: $CPU_USAGE_PERCENTAGE%
+
+Total Memory: $TOTAL_MEMORY
+Free Memory: $FREE_MEMORY
+Memory Usage Percentage: $MEMORY_USAGE_PERCENTAGE%
+
+Disk Size: $DISK_SIZE
+Free Disk: $FREE_DISK
+Disk Usage Percentage: $DISK_USAGE_PERCENTAGE%
+EOF
